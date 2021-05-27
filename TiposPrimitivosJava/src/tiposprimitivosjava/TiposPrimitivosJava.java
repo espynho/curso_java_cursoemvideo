0b4cd8416b47bea6ef69008b3ab67b3a82b8fcd2
fabio <espynho23@gmail.com>
2021-05-27 18:34:36 -0300

    tipos primitivos

---
+++ TiposPrimitivosJava/src/tiposprimitivosjava/TiposPrimitivosJava.java
@@ -0,0 +1,22 @@
+/*
+ * Tipos de variaveis
+ */
+package tiposprimitivosjava;
+
+/**
+ *
+ * @author fabinho
+ */
+public class TiposPrimitivosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 3; // tipo inteiro normal
+        int idade0 = (int) 3; // tipo typecast
+        Integer idade1 = new Integer(3); // usando classe
+        
+    }
+    
+}
