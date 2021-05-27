78043f901971a487c7b1d059c2105585531c880a
fabio <espynho23@gmail.com>
2021-05-27 18:37:46 -0300

    tipos primitivos

--- TiposPrimitivosJava/src/tiposprimitivosjava/TiposPrimitivosJava.java
+++ TiposPrimitivosJava/src/tiposprimitivosjava/TiposPrimitivosJava.java
@@ -15,7 +15,7 @@ public class TiposPrimitivosJava {
     public static void main(String[] args) {
         int idade = 3; // tipo inteiro normal
         int idade0 = (int) 3; // tipo typecast
-        Integer idade1 = new Integer(3); // usando classe
+        Integer idade1 = new Integer(3); // usando classe Wrapper Class
         
     }
     
