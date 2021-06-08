03913541896a0be4a0bb5da3160700d2ba68d6b7
fabio <espynho23@gmail.com>
2021-06-07 23:51:17 -0300

    numeros aleatórios

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -20,17 +20,31 @@ public class OperadoresAritmeticos {
 //        float m = (n1 + n2) / 2;
 //        System.out.println("A média é " + m);
         
-        int numero = 5;
+//        int numero = 5;
 //        numero++;
-        System.out.println(numero);
-        int valor = 5 + --numero;
-        System.out.println(valor);
-        System.out.println(numero);
+//        System.out.println(numero);
+//        int valor = 5 + --numero;
+//        System.out.println(valor);
+//        System.out.println(numero);
+//        
+//        // atribuição
+//        int x = 4;
+//        x *= 2;
+//        System.out.println(x);
+//    float v = 8.9f;
+//    int arredondamento = (int)Math.floor(v); // arrendonda para baixo
+//        System.out.println(arredondamento);
+//    int arredondamento1 = (int)Math.ceil(v); // arrendonda para cima
+//        System.out.println(arredondamento1);
+//    int arredondamento2 = (int)Math.round(v); // arrendonda normal
+//        System.out.println(arredondamento1);
+    double aleatorio = Math.random();
+    int n = (int)(5 + aleatorio *(10 - 5)); // método Guanabara
+    int n1 = (int) ((aleatorio )* 10)+1 ; // método encontrado na net
+        System.out.println(n1);
+    
+    
         
-        // atribuição
-        int x = 4;
-        x *= 2;
-        System.out.println(x);
     }
 
 }
